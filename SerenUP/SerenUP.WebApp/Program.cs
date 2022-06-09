d6c1a5b72a46ee1ed120c442684c7f4195609e7f
Manuel-Cestaro <manuel.cestaro01@gmail.com>
2022-06-09 11:41:47 +0200

    add google and facebook aut

--- SerenUP/SerenUP.WebApp/Program.cs
+++ SerenUP/SerenUP.WebApp/Program.cs
@@ -1,14 +1,31 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using SerenUP.WebApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("SerenUPWebAppContextConnection") ?? throw new InvalidOperationException("Connection string 'SerenUPWebAppContextConnection' not found.");
+var services = builder.Services;
+var configuration = builder.Configuration;
 
 builder.Services.AddDbContext<SerenUPWebAppContext>(options =>
     options.UseSqlServer(connectionString));;
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<SerenUPWebAppContext>();;
+
+//Google authentication
+services.AddAuthentication().AddGoogle(googleOptions =>
+{
+    googleOptions.ClientId = configuration["Authentication:Google:ClientId"];
+    googleOptions.ClientSecret = configuration["Authentication:Google:ClientSecret"];
+});
+
+//Facebook authentication
+services.AddAuthentication().AddFacebook(facebookOptions =>
+{
+    facebookOptions.AppId = configuration["Authentication:Facebook:AppId"];
+    facebookOptions.AppSecret = configuration["Authentication:Facebook:AppSecret"];
+});
 
 // Add services to the container.
 builder.Services.AddRazorPages();
