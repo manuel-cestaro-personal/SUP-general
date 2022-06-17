e3aa7d3fb8946f723981ea481bc48522c8ec36e0
Manuel-Cestaro <manuel.cestaro01@gmail.com>
2022-06-17 12:17:43 +0200

    add optional fields

--- SerenUP.Intranet/SerenUP.Intranet/Areas/Identity/Data/SerenUPIntranetContext.cs
+++ SerenUP.Intranet/SerenUP.Intranet/Areas/Identity/Data/SerenUPIntranetContext.cs
@@ -68,8 +68,8 @@ public class ApplicationUser : IdentityUser<Guid>
 {
     public string Surname { get; set; }
     public string Name { get; set; }
-    public string Address { get; set; }
-    public string City { get; set; }
+    public string? Address { get; set; }
+    public string? City { get; set; }
 }
 
 public class ApplicationRole : IdentityRole<Guid>
