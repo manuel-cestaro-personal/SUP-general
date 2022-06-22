4fea2fa81d71b639dc0bc7c0303ce5bb752dbe72
Umbertobf17 <umbertobf17@gmail.com>
2022-06-22 10:13:41 +0200

    Aggiunti singleton e eliminate pagine forecast

---
+++ SerenUP.Intranet/SerenUP.ShopAPI/Controllers/GetWatchDetailController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SerenUP.ShopAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetWatchDetailController : ControllerBase
+    {
+    }
+}
--- SerenUP.Intranet/SerenUP.ShopAPI/Program.cs
+++ SerenUP.Intranet/SerenUP.ShopAPI/Program.cs
@@ -1,3 +1,6 @@
+using SerenUP.Services.Interfaces;
+using SerenUP.Services.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IWatchService, WatchService>();
 
 var app = builder.Build();
 
