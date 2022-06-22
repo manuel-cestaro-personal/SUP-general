5411480d79d731c62bf5f02f21012c887c441276
manuel-cestaro-personal <83578640+manuel-cestaro-personal@users.noreply.github.com>
2022-06-22 23:19:51 +0200

    add call to api add information to page add odio e rancore to life

---
+++ SerenUP/SerenUP.WebApp/Models/WatchDetail.cs
@@ -0,0 +1,13 @@
+﻿namespace SerenUP.WebApp.Models
+{
+    public class WatchDetail
+    {
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+        public string Color { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+        public string Number { get; set; }
+        public int Quantity { get; set; }
+    }
+}
--- SerenUP/SerenUP.WebApp/Pages/Shop/Details/Details_Orologio.cshtml.cs
+++ SerenUP/SerenUP.WebApp/Pages/Shop/Details/Details_Orologio.cshtml.cs
@@ -1,12 +1,56 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using SerenUP.WebApp.Models;
 
 namespace SerenUP.WebApp.Pages.Shop.Details
 {
     public class Details_OrologioModel : PageModel
     {
-        public void OnGet()
+        private readonly HttpClient _client;
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<Details_OrologioModel> _logger;
+        private readonly string WatchDetail;
+        private readonly string ShopAPI;
+
+        public Details_OrologioModel(IConfiguration configuration, ILogger<Details_OrologioModel> logger)
         {
+            _client = new HttpClient();
+            _configuration = configuration;
+            ShopAPI = _configuration.GetSection("HttpUrls")["ShopAPI"];
+            WatchDetail = _configuration.GetSection("HttpUrls")["WatchDetail"];
+            _client.BaseAddress = new Uri(ShopAPI + WatchDetail);
+            _logger = logger;
+        }
+
+        public WatchDetail Watch { get; set; }
+        public string ShipmentDate { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string path)
+        {
+            ShipmentDate = DateTime.Now.AddMonths(1).ToLongDateString();
+            try
+            {
+                HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, path);
+                HttpResponseMessage response = await _client.SendAsync(requestMessage);
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    Watch = JsonConvert.DeserializeObject<WatchDetail>(content);
+                    return Page();
+                }
+                else
+                {
+                    return Page();
+                    //return RedirectToPage("/Index");
+                    //page popup prodotto non disponibile 
+                }
+            }
+            catch (Exception)
+            {
+                return RedirectToPage("/Index");
+
+            }
         }
     }
 }
