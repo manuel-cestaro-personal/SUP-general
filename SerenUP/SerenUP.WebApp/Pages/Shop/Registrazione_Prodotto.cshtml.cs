64e98741eb94881dc158a6cd8f8dd662d144337a
andrea01-trevi <andrea.trevisan@stud.tecnicosuperiorekennedy.it>
2022-06-22 14:28:41 +0200

    version 1.2

---
+++ SerenUP/SerenUP.WebApp/Pages/Shop/Registrazione_Prodotto.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SerenUP.WebApp.Pages.Shop
+{
+    public class Registrazione_ProdottoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
