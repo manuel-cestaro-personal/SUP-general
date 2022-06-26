b78c089e1d52756bf77b8d5423e7487b43cf1d7d
Manuel-Cestaro <manuel.cestaro01@gmail.com>
2022-06-27 00:04:16 +0200

    add accessory detail page with get call

---
+++ SerenUP/SerenUP.WebApp/Models/AccessoryDetail.cs
@@ -0,0 +1,11 @@
+﻿namespace SerenUP.WebApp.Models
+{
+    public class AccessoryDetail
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string Color { get; set; }
+        public int Quantity { get; set; }
+    }
+}
