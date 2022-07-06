917c8235f76367c4c6b47a7cdc39f8bfe92bff8b
UmbertoEspositoLEF <umberto.esposito@lef-digital.com>
2022-07-06 15:54:47 +0200

    Creato backend pagina "ProductList"

--- SerenUP.Intranet/SerenUP.Intranet/Pages/ProductList.cshtml.cs
+++ SerenUP.Intranet/SerenUP.Intranet/Pages/ProductList.cshtml.cs
@@ -1,12 +1,86 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using SerenUP.ApplicationCore.Entities;
+using SerenUP.Intranet.Data;
 
 namespace SerenUP.Intranet.Pages
 {
+    [Authorize]
     public class ProductListModel : PageModel
     {
-        public void OnGet()
+        private readonly ILogger<ProductListModel> _logger;
+        private readonly HttpClient _client;
+        private readonly IConfiguration _configuration;
+        private readonly string ShopAPI;
+
+        public ProductListModel(UserManager<ApplicationUser> userManager, ILogger<ProductListModel> logger, IConfiguration configuration)
         {
+            _logger = logger;
+            _client = new HttpClient();
+            _configuration = configuration;
+            ShopAPI = _configuration.GetSection("HttpUrls")["ShopAPI"];
+        }
+        public IEnumerable<Accessory> AccessoryList { get; set; }
+        public IEnumerable<Watch> WatchList { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            try
+            {
+                Uri path1 = new Uri(ShopAPI + WatchList);
+                HttpRequestMessage requestMessage1 = new HttpRequestMessage(HttpMethod.Get, path1);
+                HttpResponseMessage response1 = await _client.SendAsync(requestMessage1);
+                if (response1.IsSuccessStatusCode)
+                {
+                    string content = await response1.Content.ReadAsStringAsync();
+                    WatchList = JsonConvert.DeserializeObject<IEnumerable<Watch>>(content);
+                    if (WatchList != null)
+                    {
+                        foreach (var watch in WatchList)
+                        {
+                            watch.Link = "/Pictures/Orologi/" + watch.Model + "/" + watch.Color + ".png";
+                        }
+                    }
+                    _logger.LogInformation($"WebApp: Magazzino - {response1.StatusCode} \n{response1.RequestMessage.Method} \n{response1.RequestMessage.RequestUri} \n- {DateTime.Now}");
+                }
+                else
+                {
+                    _logger.LogInformation($"WebApp: Magazzino - {response1.StatusCode} \n{response1.RequestMessage.Method} \n{response1.RequestMessage.RequestUri} \n- {DateTime.Now}");
+                    return RedirectToPage("/Index");
+                }
+
+                Uri path2 = new Uri(ShopAPI + AccessoryList);
+                HttpRequestMessage requestMessage2 = new HttpRequestMessage(HttpMethod.Get, path2);
+                HttpResponseMessage response2 = await _client.SendAsync(requestMessage2);
+                if (response2.IsSuccessStatusCode)
+                {
+                    string content = await response2.Content.ReadAsStringAsync();
+                    AccessoryList = JsonConvert.DeserializeObject<IEnumerable<Accessory>>(content);
+                    if (AccessoryList != null)
+                    {
+                        foreach (var accessory in AccessoryList)
+                        {
+                            accessory.Link = "/Pictures/Accessori/" + accessory.Name + "/" + accessory.Color + ".png";
+                        }
+                    }
+                    _logger.LogInformation($"WebApp: Magazzino - {response2.StatusCode} \n{response2.RequestMessage.Method} \n{response2.RequestMessage.RequestUri} \n- {DateTime.Now}}");
+                }
+                else
+                {
+                    _logger.LogInformation($"WebApp: Magazzino - {response2.StatusCode} \n{response2.RequestMessage.Method} \n{response2.RequestMessage.RequestUri} \n- {DateTime.Now}");
+                    return RedirectToPage("/Index");
+                }
+
+                return Page();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation($"WebApp: Magazzino - {ex.Message} - {DateTime.Now}");
+                return RedirectToPage("/Index");
+            }
         }
     }
 }
