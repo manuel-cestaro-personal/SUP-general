daa7dfab49becb4cb642bf2956ceccac6b25fa2f
ChiaraZedde-code <chiara.zedde@stud.tecnicosuperiorekennedy.it>
2022-07-07 17:04:41 +0200

    Aggiunta pagina "Order List"

---
+++ SerenUP.Intranet/SerenUP.Intranet/Pages/OrderList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SerenUP.Intranet.Pages
+{
+    public class OrderListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
