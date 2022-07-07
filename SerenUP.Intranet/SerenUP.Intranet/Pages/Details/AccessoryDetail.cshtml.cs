b383bf722669ee419360d9946bbdb4ccdeff43bd
AlessandroAmenduni <alessandro.amenduni2934@gmail.com>
2022-07-07 12:41:37 +0200

    Dettaglio accessorio

--- SerenUP.Intranet/SerenUP.Intranet/Pages/Details/AccessoryDetail.cshtml.cs
+++ SerenUP.Intranet/SerenUP.Intranet/Pages/Details/AccessoryDetail.cshtml.cs
@@ -1,12 +1,55 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using SerenUP.ApplicationCore.Entities;
 
 namespace SerenUP.Intranet.Pages
 {
     public class AccessoryDetailModel : PageModel
     {
-        public void OnGet()
+        private readonly HttpClient _client;
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<AccessoryDetailModel> _logger;
+        private readonly string accessoryDetail;
+        private readonly string shopAPI;
+
+        public AccessoryDetailModel(IConfiguration configuration, ILogger<AccessoryDetailModel> logger)
         {
+            _client = new HttpClient();
+            _configuration = configuration;
+            shopAPI = _configuration.GetSection("HttpUrls")["ShopAPI"];
+            accessoryDetail = _configuration.GetSection("HttpUrls")["AccessoryDetail"];
+            _client.BaseAddress = new Uri(shopAPI + accessoryDetail);
+            _logger = logger;
+        }
+        public AccessoryDetail Accessory { get; set; }
+        public async Task<IActionResult> OnGetAsync(string name, string color)
+        {
+            
+            try
+            {
+                string path = name + "/" + color;
+                HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, path);
+                HttpResponseMessage response = await _client.SendAsync(requestMessage);
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    Accessory = JsonConvert.DeserializeObject<AccessoryDetail>(content);
+                    Accessory.Link = "/Pictures/Accessori/" + Accessory.Name + "/" + Accessory.Color + ".png";
+                    return Page();
+                }
+                else
+                {
+                    _logger.LogInformation($"WebApp: AccessoryDetail page - {response.StatusCode} \n{response.RequestMessage.Method} \n{response.RequestMessage.RequestUri} \n- {DateTime.Now} - {User.Identity.Name}");
+                    return RedirectToPage("./Prodotto_non_trovato");
+                    //page popup prodotto non disponibile 
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation($"WebApp: AccessoryDetail page - {ex.Message} - {DateTime.Now} - {User.Identity.Name}");
+                return RedirectToPage("/Index");
+            }
         }
     }
 }
